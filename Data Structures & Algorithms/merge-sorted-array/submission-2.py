a1037a2c6a93ef37d6960db3302ca89325a3cac3
Chris Ryan <chrisryan2210@gmail.com>
2026-08-13 16:15:33 +0100

    Add: merge-sorted-array - submission-2

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-2.py
@@ -0,0 +1,63 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+
+        while i >= 0 and j >=0:
+            if nums1[i] >= nums2[j]:
+                nums1[k] = nums1[i]
+                i-=1
+            else:
+                nums1[k] = nums2[j]
+                nums2.pop()
+                j-=1
+            k-=1
+            
+        for l in range(len(nums2) - 1, -1, -1):
+            nums1[k] = nums2[l]
+            l-=1
+            k-=1
+        
+
+
+
+
+
+        # ## USING MERGE SORT METHOD -- NOT OPTIMAL
+        # # use merge sort algo without the recursion
+        # # this very similar to merging two sorted linked lists
+        
+        # # store base arrays to grab values from below
+        # L = nums1[:m].copy()
+        # R = nums2.copy()
+        
+        # # three ptrs 
+        # i = 0 # for nums1
+        # j = 0 # for nums2
+        # k = 0 # in merge_sort recrusive it would be k = s but here just nums1[0] ->
+
+        # # nums1 is always bigger but still need to check if nums2 is not empty
+        # while i < len(L) and j < len(R):
+        #     if L[i] <= R[j]:
+        #         nums1[k] = L[i]
+        #         i+=1
+        #     else:
+        #         nums1[k] = R[j]
+        #         j+=1
+        #     k+=1
+        #     print(nums1[k])
+        
+        # while i < len(L):
+        #     nums1[k] = L[i]
+        #     i+=1
+        #     k+=1
+        
+        # while j < len(R):
+        #     nums1[k] = R[j]
+        #     j+=1
+        #     k+=1
